bd06a29e2ad599b23569a58c8e182ea3739c3d17
Guppy-ZEN <39458650+g-zen@users.noreply.github.com>
2023-08-09 06:55:40 +0900

    必殺技ゲージのUIを仮で作成

---
+++ Assets/Scripts/SpecialGauge/SpecialGaugeUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpecialGaugeUI : MonoBehaviour
+{
+    [Header("Sliderの参照")]
+    [SerializeField] private Slider sliderUI;
+
+    [Header("必殺技ゲージの最大値")]
+    [SerializeField] private int maxGaugeValue = 100;
+
+    [Header("ゲージの初期値")]
+    [SerializeField] private int initialGaugeValue = 0;
+
+    void Start()
+    {
+        sliderUI.maxValue = maxGaugeValue;
+        sliderUI.value = initialGaugeValue;
+    }
+
+    /// <summary>
+    /// 必殺技ゲージの値を設定します。
+    /// </summary>
+    /// <param name="gaugeValue">設定したいゲージの値</param>
+    public void SetGaugeValue(int gaugeValue)
+    {
+        sliderUI.value = gaugeValue;
+    }
+}
